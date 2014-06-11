f06e38d203c0972605c35cfb89ff0ab7c1ab856b
BlaTest2014 Gruppe3 <BlaTest2014@outlook.de>
2014-06-11 18:04:19 +0200

    SQL Injection vulnerability fix in info.php

--- E'NOTE/info.php
+++ E'NOTE/info.php
@@ -1,9 +1,9 @@
 <?php 
 	include 'data.php';
-	mysql_query("UPDATE ".$_POST["pname"]." SET email = '".$_POST["mail"]."' WHERE id = '".$_POST["id"]."'");
-	mysql_query("UPDATE ".$_POST["pname"]." SET realname = '".$_POST["rname"]."' WHERE id = '".$_POST["id"]."'");
-	mysql_query("UPDATE ".$_POST["pname"]." SET telephone = '".$_POST["phone"]."' WHERE id = '".$_POST["id"]."'");
-	mysql_query("UPDATE ".$_POST["pname"]." SET address = '".$_POST["address"]."' WHERE id = '".$_POST["id"]."'");
-	mysql_query("UPDATE ".$_POST["pname"]." SET name = '".$_POST["name"]."' WHERE id = '".$_POST["id"]."'"); 
+	mysql_query("UPDATE ".mysql_real_escape_string($_POST["pname"])." SET email = '".mysql_real_escape_string($_POST["mail"])."' WHERE id = '".mysql_real_escape_string($_POST["id"])."'");
+	mysql_query("UPDATE ".mysql_real_escape_string($_POST["pname"])." SET realname = '".mysql_real_escape_string($_POST["rname"])."' WHERE id = '".mysql_real_escape_string($_POST["id"])."'");
+	mysql_query("UPDATE ".mysql_real_escape_string($_POST["pname"])." SET telephone = '".mysql_real_escape_string($_POST["phone"])."' WHERE id = '".mysql_real_escape_string($_POST["id"])."'");
+	mysql_query("UPDATE ".mysql_real_escape_string($_POST["pname"])." SET address = '".mysql_real_escape_string($_POST["address"])."' WHERE id = '".mysql_real_escape_string($_POST["id"])."'");
+	mysql_query("UPDATE ".mysql_real_escape_string($_POST["pname"])." SET name = '".mysql_real_escape_string($_POST["name"])."' WHERE id = '".mysql_real_escape_string($_POST["id"])."'"); 
 	mysql_close($con);
 ?>
